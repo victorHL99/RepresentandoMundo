22711464629e35061d27480ec455b9209a7404c5
victorHL99 <victorlessa.dev@gmail.com>
2023-01-11 14:00:24 -0300

    feat: :sparkles: create test for class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        User newUser = new User();
+        Matter matter = new Matter();
+
+        newUser.setName("Victor Hugo");
+        newUser.setEmail("victor@teste.com");
+        newUser.setPosition("Aluno");
+        newUser.setUniversity("Universidade Federal de Lavras");
+        newUser.setPeriod(8);
+
+        newUser.getInfoUser();
+        matter.getAllMatters();
+
     }
 }
